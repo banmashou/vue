1101c651a2ae782c7b55404bbc58a6007c15230c
banmashou <agcwhyinuo@sina.com>
2023-08-01 22:18:16 +0800

    修复alias别名错误问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import alias from './vite/alias'
+import { parseEnv } from './vite/util'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-    // 路径别名
-    resolve: {
-        alias,
-    },
-})
+// export default defineConfig({
+//     plugins: [vue()],
+//     // 路径别名
+//     resolve: {
+//         alias,
+//     },
+// })
+
+export default ({ command, mode }: ConfigEnv) => {
+    const isBuild = command === 'build'
+    const root = process.cwd()
+    const env = loadEnv(mode, root)
+    parseEnv(env)
+    return {
+        plugins: [vue()],
+        // 路径别名
+        resolve: {
+            alias,
+        },
+    }
+}
---
+++ vite/util.ts
@@ -0,0 +1,1 @@
+export function parseEnv(env: Record<string, string>) {}
