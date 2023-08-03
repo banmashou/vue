ab29f604b73cdaf892eb986257eba990282557e4
banmashou <agcwhyinuo@sina.com>
2023-08-03 14:48:38 +0800

    转换env环境变量类型

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import alias from './vite/alias'
 import { parseEnv } from './vite/util'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//     plugins: [vue()],
-//     // 路径别名
-//     resolve: {
-//         alias,
-//     },
-// })
-
 export default ({ command, mode }: ConfigEnv) => {
     const isBuild = command === 'build'
     const root = process.cwd()
-    const env = loadEnv(mode, root)
-    parseEnv(env)
+    const env = parseEnv(loadEnv(mode, root))
+    console.log(typeof env.VITE_SOME_KEY)
     return {
         plugins: [vue()],
         // 路径别名
