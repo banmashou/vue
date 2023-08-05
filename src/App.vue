cf6779af003a18f20781c420887b62da2b013d8d
banmashou <agcwhyinuo@sina.com>
2023-08-05 17:41:20 +0800

    import.meta.env类型支持

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+console.log(typeof import.meta.env.VITE_ROUTE_AUTOLOAD);
 </script>
 
 <template>
--- types/viteEnv.d.ts
+++ types/viteEnv.d.ts
@@ -1,5 +1,17 @@
+/**
+ * @description: env类型声明
+ *
+ * @interface ViteEnv,ImportMetaEn,ImportMeta
+ */
+
 interface ViteEnv {
     VITE_SOME_KEY: number
     VITE_ROUTE_AUTOLOAD: boolean
     VITE_API_URL: string
+}
+
+interface ImportMetaEnv extends ViteEnv {}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
 }
