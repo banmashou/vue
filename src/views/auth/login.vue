121655d901f55cf6a9132d90471f51530346192c
banmashou <agcwhyinuo@sina.com>
2023-08-08 01:18:44 +0800

    表单基本设置

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -7,8 +7,14 @@
 		<div class="w-[720px] bg-white -translate-y-16 grid grid-cols-2">
 			<div class="p-6">
 				<h2 class="text-center text-gray-700 text-lg">会员登录</h2>
+				<div>
+					<input type="text" placeholder="请输入手机号或邮箱"
+						class="border w-full rounded-sm py-1 px-2 outline-none ring-1 ring-offset-2 border-gray-200 placeholder:text-xs">
+				</div>
 			</div>
-			<div>img</div>
+			<div>
+				<img src="/images/login.jpg" class="h-96 w-full object-cover">
+			</div>
 		</div>
 	</div>
 </template>
