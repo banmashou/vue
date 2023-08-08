bc8d9fa7ed981a076500251f567c7062ebf4209d
banmashou <agcwhyinuo@sina.com>
2023-08-09 00:02:11 +0800

    完成全局组件注册

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,6 @@
 import { App } from 'vue'
 import { setupTailwindcss } from './tailwindcss'
+import _ from 'lodash'
 
 /**
  * @module 插件工具
@@ -7,5 +8,19 @@ import { setupTailwindcss } from './tailwindcss'
  * @param {App} app
  */
 export function setupPlugins(app: App) {
+    autoRegisterComponent(app)
     setupTailwindcss()
+}
+
+/**
+ * @module 自动注册组件
+ *
+ * @param {App} app
+ */
+function autoRegisterComponent(app: App) {
+    const components: Record<string, any> = import.meta.glob('../components/form/*.vue', { eager: true })
+    Object.keys(components).forEach((key) => {
+        const name = key.split('/').pop()?.split('.').shift() as string
+        app.component(_.camelCase(name), components[key].default)
+    })
 }
--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,8 +1,4 @@
-<script setup lang="ts">
-import bmInput from '@/components/form/bm-input.vue'
-import bmButton from '@/components/form/bm-button.vue'
-import bmLink from '@/components/form/bm-link.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
     <div class="bg-slate-300 h-screen flex justify-center items-center p-5">
@@ -10,12 +6,9 @@ import bmLink from '@/components/form/bm-link.vue'
             <div class="p-6">
                 <h2 class="text-center text-gray-700 text-lg mt-3">会员登录</h2>
                 <div class="mt-8">
-                    <!-- <input type="text" placeholder="请输入手机号或邮箱" class="bm-input" /> -->
-                    <!-- <input type="text" placeholder="请输入手机号或邮箱" class="bm-input mt-3" /> -->
                     <bmInput />
                     <bmInput class="mt-3" />
                 </div>
-                <!-- <button class="bm-button mt-5">登录</button> -->
                 <bmButton class="mt-5" />
                 <div class="flex gap-2 justify-center mt-5">
                     <bmLink />
