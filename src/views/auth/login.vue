49c38c424d29f9e24069964ed05aebca26977e7a
banmashou <agcwhyinuo@sina.com>
2023-08-08 20:21:17 +0800

    完成PC端登录界面布局

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -4,10 +4,16 @@
     <div class="bg-slate-300 h-screen flex justify-center items-center">
         <div class="w-[720px] bg-white -translate-y-16 grid grid-cols-2 rounded-md shadow-md overflow-hidden">
             <div class="p-6">
-                <h2 class="text-center text-gray-700 text-lg">会员登录</h2>
+                <h2 class="text-center text-gray-700 text-lg mt-3">会员登录</h2>
                 <div class="mt-8">
                     <input type="text" placeholder="请输入手机号或邮箱" class="bm-input" />
                     <input type="text" placeholder="请输入手机号或邮箱" class="bm-input mt-3" />
+                </div>
+                <button class="bm-button mt-5">登录</button>
+                <div class="flex gap-2 justify-center mt-5">
+                    <a href class="text-xs text-gray-700">网站首页</a>
+                    <a href class="text-xs text-gray-700">会员注册</a>
+                    <a href class="text-xs text-gray-700">找回密码</a>
                 </div>
             </div>
             <div>
@@ -20,5 +26,8 @@
 <style lang="scss">
 .bm-input {
     @apply border w-full rounded-sm py-1 px-2 outline-none border-gray-200 placeholder:text-xs focus:ring-2 ring-offset-2 ring-violet-600 duration-300 focus:border-white;
+}
+.bm-button {
+    @apply bg-violet-700 text-white w-full py-2 rounded-md hover:bg-violet-500 duration-300;
 }
 </style>
