5b2ab7c1cfeff07ad60518c9942c9c542959d8cc
banmashou <agcwhyinuo@sina.com>
2023-08-14 23:56:30 +0800

    菜单布局

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts"></script>
 
 <template>
-    <router-view />
+    <div class="admin h-screen w-screen flex">
+        <div class="menu w-[200px] bg-gray-800 p-4">
+            <div class="logo text-gray-300 flex items-center">
+                <i class="fas fa-robot text-fuchsia-300 mr-2 text-[25px]"></i>
+                <span class="text-md">斑马兽</span>
+            </div>
+
+            <!-- 菜单 -->
+            <div class="left-container">
+                <dl class="text-gray-300 text-sm">
+                    <dt class="text-md mt-6">
+                        <i class="fas fa-file-excel mr-2"></i>
+                        <span class="text-md">错误页面</span>
+                    </dt>
+                    <dd
+                        class="py-3 pl-4 my-2 bg-violet-700 text-white rounded-md cursor-pointer hover:bg-violet-500 duration-300"
+                    >
+                        404错误
+                    </dd>
+                    <dd class="py-3 pl-4 my-2 hover:bg-violet-500 duration-300 cursor-pointer rounded-md">500错误</dd>
+                </dl>
+            </div>
+        </div>
+        <div class="content flex-1 bg-gray-200">
+            <router-view />
+        </div>
+    </div>
 </template>
 
 <style lang="scss"></style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -4,10 +4,10 @@
     <div class="h-screen w-screen bg-gray-700 flex flex-col justify-center items-center">
         <img src="/images/bm.jpg" class="w-40 h-40 object-cover rounded-full border-8 border-white mb-5" />
         <div>
-            <router-link :to="{ name: 'admin' }" class="bm-button">后台管理</router-link>
+            <router-link :to="{ name: 'admin.home' }" class="bm-button">后台管理</router-link>
             <router-link :to="{ name: 'login' }" class="bm-button ml-3 bm-danger">用户登录</router-link>
         </div>
-        <div class="text-gray-300 text-xs font-mono border-spacing-1 drop-shadow-md mt-6">欢迎使用斑马兽前端脚手架</div>
+        <div class="text-gray-300 text-xs font-mono drop-shadow-md mt-6">欢迎使用斑马兽前端脚手架</div>
     </div>
 </template>
 
