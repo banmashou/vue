379cbcee675e44a055e624d7d43cbe22317fb316
banmashou <agcwhyinuo@sina.com>
2023-08-18 01:01:46 +0800

    后台首页面板信息

--- src/views/admin/home.vue
+++ src/views/admin/home.vue
@@ -2,8 +2,22 @@
 
 <template>
     <div class="grid grid-flow-col gap-3">
-        <el-card shadow="hover" :body-style="{ padding: '20px' }" v-for="n of 4">
-            <template #header></template>
+        <el-card shadow="hover" :body-style="{ padding: '20px' }" v-for="n of 4" class="cursor-pointer">
+            <template #header>
+                <div class="flex justify-between items-center">
+                    用户数
+                    <el-tag type="danger" size="mini" effect="dark">月</el-tag>
+                </div>
+            </template>
+
+            <section class="flex mt-3 justify-between items-center">
+                <span class="text-3xl">$29322</span>
+                <i class="fas fa-ad text-violet-500 text-5xl"></i>
+            </section>
+            <section class="text-base mt-6 flex justify-between">
+                总人数
+                <span class>200人</span>
+            </section>
         </el-card>
     </div>
 </template>
