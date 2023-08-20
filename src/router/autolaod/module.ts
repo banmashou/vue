7bcf0b9619f4c5338cd63379b69c71f894387426
banmashou <agcwhyinuo@sina.com>
2023-08-20 21:24:04 +0800

    注册路由模块

--- src/router/autolaod/module.ts
+++ src/router/autolaod/module.ts
@@ -3,4 +3,12 @@
  *
  * @export
  */
-export default function autoloadModuleRoutes() {}
+import { RouteRecordRaw } from 'vue-router'
+export default function autoloadModuleRoutes() {
+    const modules: Record<string, any> = import.meta.glob('../modules/**/*.ts', { eager: true })
+    const routes = [] as RouteRecordRaw[]
+    Object.keys(modules).forEach((key) => {
+        routes.push(modules[key].default)
+    })
+    return routes
+}
---
+++ src/views/errors/404.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="bg-gradient-to-r from-purple-300 to-blue-200">
+        <div class="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
+            <div class="bg-white shadow overflow-hidden sm:rounded-lg pb-8">
+                <div class="border-t border-gray-200 text-center pt-8">
+                    <h1 class="text-9xl font-bold text-purple-400 px-20">404</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss"></style>
