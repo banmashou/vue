e5835a1583d424548078e58ea8e6513b6edf8aa0
banmashou <agcwhyinuo@sina.com>
2023-08-22 00:13:36 +0800

    优化快速导航样式

--- src/layouts/admin/historyLink.vue
+++ src/layouts/admin/historyLink.vue
@@ -6,13 +6,25 @@ const links = ref<{ title: string }[]>([
     { title: '订单列表' },
     { title: '用户管理' },
     { title: '销售总额' },
+    { title: '编辑器' },
+    { title: '订单列表' },
+    { title: '用户管理' },
+    { title: '销售总额' },
+    { title: '编辑器' },
+    { title: '订单列表' },
+    { title: '用户管理' },
+    { title: '销售总额' },
 ])
 </script>
 
 <template>
-    <div class="hidden md:block">
-        <div class="m-5 grid grid-flow-col gap-3 justify-start">
-            <a v-for="link of links" class="bg-white rounded-md py-2 px-3 text-sm text-gray-600">
+    <div class="hidden md:block bg-gray-50 p-3 border-t border-b shadow-sm">
+        <div class="grid grid-flow-col gap-3 justify-start">
+            <a
+                href
+                v-for="link of links"
+                class="border hover:bg-violet-600 hover:text-white duration-300 bg-white rounded-sm shadow-sm py-2 px-3 text-sm text-gray-600"
+            >
                 {{ link.title }}
                 <i class="fas fa-times ml-1"></i>
             </a>
--- src/router/modules/admin.ts
+++ src/router/modules/admin.ts
@@ -3,6 +3,7 @@ import { RouteRecordRaw } from 'vue-router'
 export default {
     name: 'admin',
     path: '/admin',
+    redirect: '/admin/home',
     component: () => import('@/layouts/admin.vue'),
     meta: { auth: true },
     children: [{ name: 'admin.home', path: 'home', component: () => import('@/views/admin/home.vue') }],
