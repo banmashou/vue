1157d176544a21f04b7575c2e37965c2a6e030de
banmashou <agcwhyinuo@sina.com>
2023-08-26 20:48:26 +0800

    优化站内消息点击事件

--- src/composables/notification.vue
+++ src/composables/notification.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { nextTick, ref } from 'vue'
 
 const activeName = ref('site')
 
 const show = ref(false)
+
+nextTick(() => {
+    document.documentElement.addEventListener('click', () => {
+        show.value = false
+    })
+})
 </script>
 
 <template>
     <div class="notification">
-        <i class="fas fa-align-justify" @click="show = !show"></i>
-        <el-tabs v-model="activeName" class="lists" v-show="show">
+        <i class="fas fa-align-justify" @click.stop="show = !show"></i>
+        <el-tabs v-model="activeName" class="lists" v-show="show" @click.stop>
             <el-tab-pane label="系统通知" name="site">
                 <a href=""
                     >天氣身邊石油的確將軍考慮，準備以外一切性感傳說責任發生低價官員一段產品演出先後版主，不必專欄婚姻，義務旁邊行政獎勵之處生意人生它的哪個方法員工，客人代理畢業網際遊客我和今天風景，人力桌面許可把我給他減少歷史苗栗幾天角色我一富岡，最終謝謝，明星聽到收入，好。</a
