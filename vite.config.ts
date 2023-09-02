1930a63bbc8167266e03bfea19ed02156fee8a67
banmashou <agcwhyinuo@sina.com>
2023-09-03 04:38:08 +0800

    修复vite.config.ts报错问题

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default ({ command, mode }: ConfigEnv) => {
             alias,
         },
         server: {
+            host: '127.0.0.1',
             port: 3000,
         },
         build: {
