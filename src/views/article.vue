6ce9924a362a910d7b27b8237c3ddb16c786fa55
banmashou <agcwhyinuo@sina.com>
2023-09-02 17:46:50 +0800

    渲染文章列表

---
+++ src/views/article.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import articleApi from '@/apis/articleApi'
+
+const { data: result } = await articleApi.article()
+
+const articles = ref(result)
+</script>
+
+<template>
+    <div class="article">
+        <ul>
+            <li v-for="article of articles" :key="article.id">
+                {{ article.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.article {
+    padding: 30px;
+    ul {
+        li {
+            padding: 10px;
+            margin-bottom: 20px;
+            background: #8e44ad;
+            color: white;
+        }
+    }
+}
+</style>
