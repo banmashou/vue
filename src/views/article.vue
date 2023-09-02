9f39dfb0fd1c3c86d191fb0180750521f0129b14
banmashou <agcwhyinuo@sina.com>
2023-09-02 18:21:31 +0800

    使用gsap完成渐进动画

--- src/views/article.vue
+++ src/views/article.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
 import articleApi from '@/apis/articleApi'
+import gsap from 'gsap'
 
-const { data: result } = await articleApi.article()
-
-const articles = ref(result)
+const articles = ref()
+articleApi.article().then(({ data }) => (articles.value = data))
 
 const del = (index: number) => {
     articles.value.splice(index, 1)
+}
+
+const beforeEnter = (el) => {
+    gsap.set(el, {
+        opacity: 0,
+    })
+}
+
+const enter = (el) => {
+    gsap.to(el, {
+        opacity: 1,
+        duration: 1,
+        delay: el.dataset.index * 0.2,
+    })
 }
 </script>
 
 <template>
     <div class="article">
-        <TransitionGroup tag="ul" name="animate">
-            <li v-for="(article, index) of articles" :key="article.id" @click="del(index)">
+        <TransitionGroup appear tag="ul" name="animate" @before-enter="beforeEnter" @enter="enter">
+            <li :data-index="index" v-for="(article, index) of articles" :key="article.id" @click="del(index)">
                 {{ article.title }}
             </li>
         </TransitionGroup>
@@ -23,14 +37,17 @@ const del = (index: number) => {
 <style lang="scss" scoped>
 .article {
     padding: 30px;
+    width: 100vw;
+    height: 100vh;
     position: relative;
     background: #34495e;
     ul {
         li {
             padding: 10px;
             margin-bottom: 20px;
-            background: #8e44ad;
-            color: white;
+            background: #f1c40f;
+            color: #333;
+            border-radius: 5px;
         }
     }
 }
