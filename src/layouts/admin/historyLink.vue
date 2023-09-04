94bc19440decc56657453696c4e7a0677657fd58
banmashou <agcwhyinuo@sina.com>
2023-09-05 00:17:43 +0800

    修改历史菜单删除样式

--- src/layouts/admin/historyLink.vue
+++ src/layouts/admin/historyLink.vue
@@ -7,11 +7,17 @@ import menuService from '@/composables/menu'
         <div class="grid grid-flow-col gap-3 justify-start">
             <div
                 v-for="item of menuService.history.value"
-                class="border hover:bg-violet-600 hover:text-white duration-300 bg-white rounded-sm shadow-sm py-2 px-3 text-sm text-gray-600"
+                class="border hover:bg-violet-600 hover:text-white duration-300 bg-white rounded-sm shadow-sm py-2 px-3 text-sm text-gray-600 flex justify-between items-center cursor-pointer"
                 :class="{ '!bg-violet-600 text-white': $route.name === item.route }"
             >
                 <router-link :to="{ name: item.route }">{{ item.title }}</router-link>
-                <i class="fas fa-times ml-1" @click="menuService.removeHistoryMenu(item)"></i>
+                <icon-close-small
+                    theme="outline"
+                    size="18"
+                    class="ml-2"
+                    :class="{ 'text-white': $route.name === item.route }"
+                    @click="menuService.removeHistoryMenu(item)"
+                />
             </div>
         </div>
     </div>
